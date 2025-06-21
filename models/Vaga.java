2aa6b9ce7533745fb3de06b9807a30965d18747b
João Emanuel <joemanuelalves1@gmail.com>
2025-06-21 15:40:14 -0300

    Vaga class ok

--- models/Vaga.java
+++ models/Vaga.java
@@ -1,6 +1,8 @@
 package models;
 
-public class Vaga {
+import interfaces.StatusVaga;
+
+public class Vaga implements StatusVaga{
     private boolean ocupada;
     private PessoaAcolhida pessoa;
 
@@ -12,18 +14,24 @@ public class Vaga {
     public boolean isOcupada() {
         return ocupada;
     }
-    public void ocupar(PessoaAcolhida pessoa) {
-        this.pessoa = pessoa;
-        this.ocupada = true;
-    }
-    public void liberar() {
-        this.pessoa = null;
-        this.ocupada = false;
-    }
 
     public PessoaAcolhida getPessoa() {
         return pessoa;
     }
+    public void setPessoa(PessoaAcolhida pessoa) {
+        this.pessoa = pessoa;
+    }
 
+    @Override
+    public void ocupar(PessoaAcolhida pessoa) {
+        this.pessoa = pessoa;
+        this.ocupada = true;
+    }
+
+    @Override
+    public void liberar() {
+        this.pessoa = null;
+        this.ocupada = false;
+    }
     
 }

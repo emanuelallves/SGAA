91263930b7b845217fd0df56d9c406e0838fa4fb
João Emanuel <joemanuelalves1@gmail.com>
2025-06-21 13:42:40 -0300

    Transformando a classe Profissional em uma abstrata

--- models/Profissional.java
+++ models/Profissional.java
@@ -1,14 +1,23 @@
 package models;
 
-public class Profissional {
-    private String nome;
-    private String tipo;
-    private Contato contato;
+import java.util.ArrayList;
+import java.util.List;
 
-    public Profissional(String nome, String tipo, Contato contato) {
+public abstract class Profissional {
+    private String nome;
+    private String CPF;
+    private Contato contato;
+    private String horarioEntrada;
+    private String horarioSaida;
+    protected List<String> tarefas;
+
+    public Profissional(String nome, String CPF, Contato contato, String horarioEntrada, String horarioSaida) {
         this.nome = nome;
-        this.tipo = tipo;
+        setCPF(CPF);
         this.contato = contato;
+        this.horarioEntrada = horarioEntrada;
+        this.horarioSaida = horarioSaida;
+        tarefas = new ArrayList<>();
     }
 
     public String getNome() {
@@ -17,12 +26,16 @@ public class Profissional {
     public void setNome(String nome) {
         this.nome = nome;
     }
-
-    public String getTipo() {
-        return tipo;
+    
+    public String getCPF() {
+        return CPF;
     }
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
+    public void setCPF(String CPF) {
+        if(!CPF.matches("\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}")) {
+            System.out.println("CPF inválido");
+            return;
+        }
+        this.CPF = CPF;
     }
 
     public Contato getContato() {
@@ -32,5 +45,20 @@ public class Profissional {
         this.contato = contato;
     }
 
-    
+    public String getHorarioEntrada() {
+        return horarioEntrada;
+    }
+    public void setHorarioEntrada(String horarioEntrada) {
+        this.horarioEntrada = horarioEntrada;
+    }
+
+    public String getHorarioSaida() {
+        return horarioSaida;
+    }
+    public void setHorarioSaida(String horarioSaida) {
+        this.horarioSaida = horarioSaida;
+    }
+
+    public abstract List<String> getTarefas();
+    public abstract void adicionarTarefas(String tarefa);
 }

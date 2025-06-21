df77fa568716738433ceffa3112a7ab39188a48d
João Emanuel <joemanuelalves1@gmail.com>
2025-06-21 16:31:28 -0300

    Emergencial class ok

--- models/Emergencial.java
+++ models/Emergencial.java
@@ -1,12 +1,40 @@
 package models;
 
 public class Emergencial extends Abrigo {
-    public Emergencial(String nome, Endereco endereco, int capacidade) {
+    private int duracaoMaximaDias;
+
+    public Emergencial(String nome, Endereco endereco, int capacidade, int duracaoMaximaDias) {
         super(nome, endereco, capacidade);
+        this.duracaoMaximaDias = duracaoMaximaDias;
+    }
+
+    public int getDuracaoMaximaDias() {
+        return duracaoMaximaDias;
+    }
+    public void setDuracaoMaximaDias(int duracaoMaximaDias) {
+        this.duracaoMaximaDias = duracaoMaximaDias;
     }
 
     @Override
     public String getTipo() {
         return "Emergencial";
     }
+
+    @Override
+    public void encerrarAcolhimento(PessoaAcolhida pessoa) {
+        boolean b = true;
+        for(Vaga v : getVagas()) {
+            if(v.getPessoa().getNome() == pessoa.getNome()) {
+                v.liberar();
+                System.out.println("Vaga liberada com sucesso!");
+                b = false;
+                break;
+            }
+        }
+
+        if(b) {
+            System.out.println("Nenhuma vaga está sendo ocupada por esta pessoa!");
+        }
+    }
+
 }

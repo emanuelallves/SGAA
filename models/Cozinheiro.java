c149ff04ca0464d9222e222f2394a211b7ab5a09
João Emanuel <joemanuelalves1@gmail.com>
2025-06-21 14:01:40 -0300

    .

---
+++ models/Cozinheiro.java
@@ -0,0 +1,40 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cozinheiro extends Profissional{
+    private List<String> pratos;
+    private boolean responsavelPorCardapio;
+
+    public Cozinheiro(String nome, String CPF, Contato contato, String horarioEntrada, String horarioSaida) {
+        super(nome, CPF, contato, horarioEntrada, horarioSaida);
+        this.pratos = new ArrayList<>();
+        this.responsavelPorCardapio = false;
+    }
+
+    public List<String> getPratos() {
+        return pratos;
+    }
+    public void adicionarPrato(String prato) {
+        pratos.add(prato);
+    }
+
+    public boolean isResponsavelPorCardapio() {
+        return responsavelPorCardapio;
+    }
+    public void setResponsavelPorCardapio(boolean responsavelPorCardapio) {
+        this.responsavelPorCardapio = responsavelPorCardapio;
+    }
+
+    @Override
+    public List<String> getTarefas() {
+        return tarefas;
+    }
+
+    @Override
+    public void adicionarTarefas(String tarefa) {
+        tarefas.add(tarefa);
+    }
+    
+}

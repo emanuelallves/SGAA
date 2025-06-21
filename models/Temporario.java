a6e147aaab4f2eeb197e4fe0fb3654d68b549a6a
João Emanuel <joemanuelalves1@gmail.com>
2025-06-21 16:31:48 -0300

    Temporario class ok

--- models/Temporario.java
+++ models/Temporario.java
@@ -1,12 +1,23 @@
 package models;
 
 public class Temporario extends Abrigo {
-    public Temporario(String nome, Endereco endereco, int capacidade) {
+    private int prazoMaximoDias;
+
+    public Temporario(String nome, Endereco endereco, int capacidade, int prazoMaximoDias) {
         super(nome, endereco, capacidade);
+        this.prazoMaximoDias = prazoMaximoDias;
+    }
+
+    public int getPrazoMaximoDias() {
+        return prazoMaximoDias;
+    }
+    public void setPrazoMaximoDias(int prazoMaximoDias) {
+        this.prazoMaximoDias = prazoMaximoDias;
     }
 
     @Override
     public String getTipo() {
         return "Temporário";
     }
+
 }

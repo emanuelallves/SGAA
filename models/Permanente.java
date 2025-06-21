4ac579ef7538d3c688bc5a550caf222ae3536414
João Emanuel <joemanuelalves1@gmail.com>
2025-06-21 16:31:05 -0300

    Permanente class ok

--- models/Permanente.java
+++ models/Permanente.java
@@ -1,12 +1,23 @@
 package models;
 
 public class Permanente extends Abrigo {
-    public Permanente(String nome, Endereco endereco, int capacidade) {
+    private int tempoMinimoMeses;
+
+    public Permanente(String nome, Endereco endereco, int capacidade, int tempoMinimoMeses) {
         super(nome, endereco, capacidade);
+        this.tempoMinimoMeses = tempoMinimoMeses;
+    }
+
+    public int getTempoMinimoMeses() {
+        return tempoMinimoMeses;
+    }
+    public void setTempoMinimoMeses(int tempoMinimoMeses) {
+        this.tempoMinimoMeses = tempoMinimoMeses;
     }
 
     @Override
     public String getTipo() {
         return "Permanente";
     }
+
 }
